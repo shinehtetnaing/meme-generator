8f1ad167a4e186f743d08601f1c11ea6ac6ebce2
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-11-14 12:53:39 +0700

    controlled components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 const Main = () => {
+  const [meme, setMeme] = useState({
+    imageUrl: "https://i.imgflip.com/1bij.jpg",
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
   return (
     <main className="space-y-5 p-7">
       <div className="flex gap-5">
@@ -10,6 +26,8 @@ const Main = () => {
             placeholder="One does not simply"
             name="topText"
             className="mt-2 w-full rounded border border-gray-300 p-3"
+            onChange={handleChange}
+            value={meme.topText}
           />
         </label>
 
@@ -21,6 +39,8 @@ const Main = () => {
             placeholder="Walk into Mordor"
             name="bottomText"
             className="mt-2 w-full rounded border border-gray-300 p-3"
+            onChange={handleChange}
+            value={meme.bottomText}
           />
         </label>
       </div>
@@ -28,16 +48,12 @@ const Main = () => {
         Get a new meme image 🖼️
       </button>
       <div className="relative">
-        <img
-          src="https://i.imgflip.com/1bij.jpg"
-          alt="Meme"
-          className="w-full rounded"
-        />
+        <img src={meme.imageUrl} alt="Meme" className="w-full rounded" />
         <span className="absolute inset-x-0 top-0 z-50 py-3 text-center text-4xl font-extrabold text-white uppercase [-webkit-text-stroke:2px_black]">
-          One does not simply
+          {meme.topText}
         </span>
         <span className="absolute inset-x-0 bottom-0 z-50 py-3 text-center text-4xl font-extrabold text-white uppercase [-webkit-text-stroke:2px_black]">
-          Walk into Mordor
+          {meme.bottomText}
         </span>
       </div>
     </main>
